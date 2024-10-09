ec977b96ad94caa9df01182bdf3d279c21b2bdcc
sanguinokarol29 <108771875+sanguinokarol29@users.noreply.github.com>
2024-10-08 22:49:34 -0300

    aula 4  08/10

---
+++ informacoesGlobais.js
@@ -0,0 +1,12 @@
+const url = ‘https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json’;
+
+async function visualiarInformacoesGlobais() {
+
+    const res = await fetch(url);
+    const dados = res.json();
+    console.log(dados);
+    
+}
+
+visualizarinformacoesGlobais();
+
