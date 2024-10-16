63ea39c9ced670f4aad0c830eb08db654e4c79bc
sanguinokarol29 <108771875+sanguinokarol29@users.noreply.github.com>
2024-10-15 22:49:39 -0300

    aula 15/10

--- informacoesGlobais.js
+++ informacoesGlobais.js
@@ -4,8 +4,12 @@ async function visualizarInformacoesGlobais() {
 
     const res = await fetch(url);
     const dados = res.json();
-    console.log(dados);
+    const paragrafo = document.createElement('p');
+    paragrafo.classList.add('graficos-container_texto');
+    paragrafo.innerHTML = 'Você sabia que o mundo tem ${dados.total_pessoas_mundo} e que aproximadamente ${dados.total_pessoas_mundo} estão conectadas em alguma rede social e passam em média ${dados.tempo_medio} horas conectados.';
 
+    const container = document.getElementById('graficos-container');
+    container.appendChild(paragrafo);
 }
 
 visualizarInformacoesGlobais();
