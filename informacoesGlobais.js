06b35f9b05024de2c6d6f7611d094f7eabcfefef
sanguinokarol29 <108771875+sanguinokarol29@users.noreply.github.com>
2024-10-22 22:41:43 -0300

    aula 6 22/10

--- informacoesGlobais.js
+++ informacoesGlobais.js
@@ -5,12 +5,11 @@ async function visualizarInformacoesGlobais() {
     const res = await fetch(url);
     const dados = await res.json();
     const paragrafo = document.createElement('p');
-    paragrafo.classList.add('graficos-container_texto');
-    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${dados.total_pessoas_mundo}</span> e que aproximadamente <span>${dados.total_pessoas_mundo}</span> destas estão conectadas em alguma rede social e passam em média <span>${dados.tempo_medio} horas conectados.`;
-
+    paragrafo.classList.add('graficos-container__texto');
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${dados.total_pessoas_mundo}</span> de pessoas e que aproximadamente <span>${dados.total_pessoas_conectadas}</span> destas estão conectadas em alguma rede social e passam em média <span>${dados.tempo_medio}</span> horas conectadas.`;
+    
     const container = document.getElementById('graficos-container');
     container.appendChild(paragrafo);
 }
 
 visualizarInformacoesGlobais();
-
